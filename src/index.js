2d7a2651340932563275c349d38466f3bcec4e9b
yazakhariya <17070501.iana@gmail.com>
2023-07-01 21:04:24 +0300

    url-req-fixed

--- src/index.js
+++ src/index.js
@@ -6,34 +6,37 @@ const getUsers = () => {
   return fs.readFileSync(filePath);
 };
 
-const name = "Stacy";
-
 const server = http.createServer((req, res) => {
-  if (req.url === `/?hello=${name}`) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.write(`Hello, ${name}!\n`);
-    res.end();
-    return;
-  } else if (req.url === "/?hello") {
-    res.statusCode = 400;
-    res.setHeader("Content-Type", "text/plain");
-    res.write("Enter a name\n");
-    res.end();
-    return;
-  } else if (req.url === "/?users") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(getUsers());
-    console.log(getUsers());
-    return;
-  } else if (req.url === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
-  } else {
-    res.statusCode = 500;
-    res.end("ouch");
+  const searchParams = new URLSearchParams(req.url);
+
+  for (let [key, value] of searchParams.entries()) {
+    if (key === "/?hello") {
+      if (value) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.write(`Hello, ${value}!\n`);
+        res.end();
+        return;
+      }
+      res.statusCode = 400;
+      res.setHeader("Content-Type", "text/plain");
+      res.write("Enter a name\n");
+      res.end();
+      return;
+    } else if (key === "/?users") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(getUsers());
+      console.log(getUsers());
+      return;
+    } else if (key === "/") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Hello World\n");
+    } else {
+      res.statusCode = 500;
+      res.end("ouch");
+    }
   }
 });
 
